df430ebc3aa20607fbaadf63773f71176f9c6bad
Matyas Balcar <matyasbalcar0@gmail.com>
2026-03-04 10:46:26 +0100

    use fixed path

--- app/Knihovna/db/DB SETUP/create_db.bat
+++ app/Knihovna/db/DB SETUP/create_db.bat
@@ -2,7 +2,7 @@
 set DB_NAME=KnihovnaDB.fdb
 set DB_USER=SYSDBA
 set DB_PASS=masterkey
-set ISQL_PATH="%~dp0isql.exe"
+set ISQL_PATH="C:\fb\Firebird_5_0\isql.exe"
 
 pushd "%~dp0.."
 set "PFolder=%CD%"
@@ -14,8 +14,7 @@ if exist "%DB_PATH%" (
     echo DB ALREADY EXISTS
     exit /b
 )
-
-echo CREATING DB
+echo ===START DB===
 echo SET SQL DIALECT 3; > "%~dp0temp_create.sql"
 echo CREATE DATABASE '%DB_PATH%' USER '%DB_USER%' PASSWORD '%DB_PASS%' PAGE_SIZE 8192 DEFAULT CHARACTER SET UTF8; >> "%~dp0temp_create.sql"
 echo EXIT; >> "%~dp0temp_create.sql"
@@ -28,5 +27,4 @@ echo ADDING TABLES AND DATA
 %ISQL_PATH% "%DB_PATH%" -user %DB_USER% -password %DB_PASS% -i "%~dp0create_tables.sql"
 %ISQL_PATH% "%DB_PATH%" -user %DB_USER% -password %DB_PASS% -i "%~dp0insert_test_data.sql"
 
-echo QUERIES COMPLETED
 pause

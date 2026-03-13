531b8cde095e115bdba5f1442e8dec1e8b5cab04
Matyas Balcar <matyasbalcar0@gmail.com>
2026-03-13 16:50:23 +0100

    move searchable combobx to helper, also add tyo bookwindow

--- app/Knihovna/MainWindow.xaml.cs
+++ app/Knihovna/MainWindow.xaml.cs
@@ -20,62 +20,7 @@ namespace Knihovna
             Storyboard sb = (Storyboard)FindResource("ToastAnimation");
             sb.Begin();
         }
-        /*
-         * Had do write this, default combobox search was instant (so searchign would change the table a lot), and didnt show selected thing up top, THis intercepts that
-         * and makes it so its easily searchable without the table changing while looking for the right filter.
-         */
-        private void FilterComboBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (sender is ComboBox comboBox && comboBox.IsEditable && comboBox.IsKeyboardFocusWithin)
-            {
-                var textBox = comboBox.Template.FindName("PART_EditableTextBox", comboBox) as TextBox;
-                int caretPosition = textBox?.CaretIndex ?? 0;
 
-                string searchText = comboBox.Text;
 
-                var view = CollectionViewSource.GetDefaultView(comboBox.ItemsSource);
-                if (view != null)
-                {
-                    view.Filter = item =>
-                    {
-                        if (string.IsNullOrEmpty(searchText)) return true;
-
-                        var property = item.GetType().GetProperty(comboBox.DisplayMemberPath);
-                        var value = property?.GetValue(item)?.ToString();
-
-                        //maybe do starts_with
-                        return value != null && value.Contains(searchText, StringComparison.OrdinalIgnoreCase);
-                    };
-
-                    if (string.IsNullOrEmpty(searchText) || view.IsEmpty)
-                    {
-                        comboBox.IsDropDownOpen = false; 
-                    }
-                    else
-                    {
-                        comboBox.IsDropDownOpen = true; 
-                    }
-                }
-
-                if (textBox != null)
-                {
-                    textBox.SelectionLength = 0;
-                    textBox.CaretIndex = caretPosition;
-                }
-            }
-        }
-
-        private void FilterComboBox_DropDownClosed(object sender, EventArgs e)
-        {
-
-            if (sender is ComboBox comboBox)
-            {
-                var view = CollectionViewSource.GetDefaultView(comboBox.ItemsSource);
-                if (view != null)
-                {
-                    view.Filter = null;
-                }
-            }
-        }
     }
 }

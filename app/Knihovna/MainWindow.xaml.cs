f852ed9ceda0722dd49a62b303406f7cb57a26b7
Matyas Balcar <matyasbalcar0@gmail.com>
2026-03-13 12:37:19 +0100

    add text search to comboboxes, had to write code behind to not lookup instantly

--- app/Knihovna/MainWindow.xaml.cs
+++ app/Knihovna/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media.Animation;
 
 namespace Knihovna
@@ -16,6 +18,53 @@ namespace Knihovna
 
             Storyboard sb = (Storyboard)FindResource("ToastAnimation");
             sb.Begin();
+        }
+
+        private void FilterComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox && comboBox.IsEditable && comboBox.IsKeyboardFocusWithin)
+            {
+                var textBox = comboBox.Template.FindName("PART_EditableTextBox", comboBox) as TextBox;
+
+                int caretPosition = textBox?.CaretIndex ?? 0;
+
+                comboBox.IsDropDownOpen = true;
+
+                if (textBox != null)
+                {
+                    textBox.SelectionLength = 0;
+                    textBox.CaretIndex = caretPosition;
+                }
+
+                string searchText = comboBox.Text;
+
+                var view = CollectionViewSource.GetDefaultView(comboBox.ItemsSource);
+                if (view != null)
+                {
+                    view.Filter = item =>
+                    {
+                        if (string.IsNullOrEmpty(searchText)) return true;
+
+                        var property = item.GetType().GetProperty(comboBox.DisplayMemberPath);
+                        var value = property?.GetValue(item)?.ToString();
+
+                        return value != null && value.Contains(searchText, StringComparison.OrdinalIgnoreCase);
+                    };
+                }
+            }
+        }
+
+        private void FilterComboBox_DropDownClosed(object sender, EventArgs e)
+        {
+
+            if (sender is ComboBox comboBox)
+            {
+                var view = CollectionViewSource.GetDefaultView(comboBox.ItemsSource);
+                if (view != null)
+                {
+                    view.Filter = null;
+                }
+            }
         }
     }
 }

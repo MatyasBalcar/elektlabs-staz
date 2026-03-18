98a42a215ebe9919a92f01870dd24528a407da43
Matyas Balcar <matyasbalcar0@gmail.com>
2026-03-18 17:45:43 +0100

    small changes to ui and create_db.bat

--- app/Knihovna/db/DB SETUP/create_db.bat
+++ app/Knihovna/db/DB SETUP/create_db.bat
@@ -1,8 +1,10 @@
 @echo off
-set DB_NAME=KnihovnaDB.fdb
-set DB_USER=SYSDBA
-set DB_PASS=masterkey
-set ISQL_PATH="C:\fb\isql.exe"
+setlocal enabledelayedexpansion
+
+set "DB_NAME=KnihovnaDB.fdb"
+set "DB_USER=SYSDBA"
+set "DB_PASS=masterkey"
+set "ISQL_PATH=C:\fb\isql.exe"
 
 pushd "%~dp0.."
 set "PFolder=%CD%"
@@ -15,14 +17,26 @@ if exist "%DB_PATH%" (
     exit /b
 )
 
-echo ===START DB===
-echo CREATING DB 
+echo CREATING DB
 
 %ISQL_PATH% -user %DB_USER% -password %DB_PASS% -i "%~dp0create_db.sql"
-
+if %errorlevel% neq 0 (
+    echo Error: Failed to create the database. Error code: !errorlevel!
+    exit /b 1
+)
 
 %ISQL_PATH% "%DB_PATH%" -user %DB_USER% -password %DB_PASS% -i "%~dp0create_tables.sql"
+if %errorlevel% neq 0 (
+    echo Error: Failed to create tables. Error code: !errorlevel!
+    exit /b 1
+)
+
 %ISQL_PATH% "%DB_PATH%" -user %DB_USER% -password %DB_PASS% -i "%~dp0insert_test_data.sql"
+if %errorlevel% neq 0 (
+    echo Error: Failed to insert test data. Error code: !errorlevel!
+    exit /b 1
+)
 
 echo DB CREATED WITH DATA
+
 pause

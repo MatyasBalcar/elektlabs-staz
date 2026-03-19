1e95d2600fc62b5eb830d38ee012373dec692aa0
Matyas Balcar <matyasbalcar0@gmail.com>
2026-03-19 13:10:25 +0100

    add basic tests

---
+++ app/Knihovna.Tests/Models/DatabaseManagerReadTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using Knihovna.Models;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Knihovna.Tests
+{
+    [TestClass]
+    public class DatabaseManagerReadTests
+    {
+        private DbContextOptions<AppDbContext> _options;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            using var context = new AppDbContext(_options);
+            SeedDatabase(context);
+        }
+
+        private void SeedDatabase(AppDbContext context)
+        {
+            var langCze = new Language { LanguageID = 1, Name = "Čeština" };
+            var langEng = new Language { LanguageID = 2, Name = "English" };
+            var pub1 = new Publisher { PublisherID = 1, Name = "Argo" };
+            var natCze = new Nationality { NationalityID = 1, Name = "Czech" };
+            var author1 = new Author { AuthorId = 1, FirstName = "Karel", LastName = "Čapek", Nationality = natCze };
+
+            context.Languages.AddRange(langCze, langEng);
+            context.Publishers.Add(pub1);
+            context.Nationalities.Add(natCze);
+            context.Authors.Add(author1);
+
+            context.Books.Add(new Book
+            {
+                BookId = 1,
+                Name = "R.U.R.",
+                Language = langCze,
+                Publisher = pub1,
+                Authors = new List<Author> { author1 }
+            });
+
+            context.Books.Add(new Book
+            {
+                BookId = 2,
+                Name = "Válka s mloky",
+                Language = langCze,
+                Publisher = pub1,
+                Authors = new List<Author> { author1 }
+            });
+
+            context.SaveChanges();
+        }
+
+        [TestMethod]
+        public void GetBooks_NoFilters_ReturnsAllBooksSorted()
+        {
+            var manager = new DatabaseManager(_options);
+            var result = manager.GetBooks();
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("R.U.R.", result[0].Name);
+        }
+
+        [TestMethod]
+        public void GetBooks_FilterByName_ReturnsMatch()
+        {
+            var manager = new DatabaseManager(_options);
+            var result = manager.GetBooks(name: "Válka");
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("Válka s mloky", result[0].Name);
+        }
+
+        [TestMethod]
+        public void GetAuthors_ReturnsSortedAuthors()
+        {
+            var manager = new DatabaseManager(_options);
+            var result = manager.GetAuthors();
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("Karel Čapek", result[0].FullName);
+        }
+    }
+}
---
+++ app/Knihovna.Tests/Models/DatabaseManagerWriteAndDeleteTests.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using Knihovna.Models;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Knihovna.Tests
+{
+    [TestClass]
+    public class DatabaseManagerWriteAndDeleteTests
+    {
+        private DbContextOptions<AppDbContext> _options;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        [TestMethod]
+        public void SaveBook_NewBook_InsertsCorrectly()
+        {
+            var manager = new DatabaseManager(_options);
+            var book = new Book { Name = "Test Book", Authors = new List<Author>() };
+
+            manager.SaveBook(book);
+
+            using var context = new AppDbContext(_options);
+            Assert.AreEqual(1, context.Books.Count());
+            Assert.AreEqual("Test Book", context.Books.First().Name);
+        }
+
+        [TestMethod]
+        public void DeleteBook_RemovesBook()
+        {
+            using (var context = new AppDbContext(_options))
+            {
+                context.Books.Add(new Book { BookId = 99, Name = "Delete Me" });
+                context.SaveChanges();
+            }
+
+            var manager = new DatabaseManager(_options);
+            manager.DeleteBook(99);
+
+            using (var context = new AppDbContext(_options))
+            {
+                Assert.IsNull(context.Books.Find(99));
+            }
+        }
+
+        [TestMethod]
+        public void SaveAuthor_UpdatesExistingAuthor()
+        {
+            using (var context = new AppDbContext(_options))
+            {
+                context.Authors.Add(new Author { AuthorId = 1, FirstName = "Old", LastName = "Name" });
+                context.SaveChanges();
+            }
+
+            var manager = new DatabaseManager(_options);
+            var updated = new Author { AuthorId = 1, FirstName = "New", LastName = "Name" };
+
+            manager.SaveAuthor(updated);
+
+            using (var context = new AppDbContext(_options))
+            {
+                Assert.AreEqual("New", context.Authors.Find(1).FirstName);
+            }
+        }
+
+        [TestMethod]
+        public void DeleteAuthor_RemovesAuthorAndBooks()
+        {
+            using (var context = new AppDbContext(_options))
+            {
+                var author = new Author { AuthorId = 5, FirstName = "A", LastName = "B" };
+                var book = new Book { BookId = 10, Name = "Book", Authors = new List<Author> { author } };
+                context.Authors.Add(author);
+                context.Books.Add(book);
+                context.SaveChanges();
+            }
+
+            var manager = new DatabaseManager(_options);
+            manager.DeleteAuthor(5);
+
+            using (var context = new AppDbContext(_options))
+            {
+                Assert.AreEqual(0, context.Authors.Count());
+                Assert.AreEqual(0, context.Books.Count());
+            }
+        }
+
+        [TestMethod]
+        public void GetAllNationalities_ReturnsSorted()
+        {
+            using (var context = new AppDbContext(_options))
+            {
+                context.Nationalities.Add(new Nationality { Name = "USA" });
+                context.Nationalities.Add(new Nationality { Name = "Albania" });
+                context.SaveChanges();
+            }
+
+            var manager = new DatabaseManager(_options);
+            var result = manager.GetAllNationalities();
+
+            Assert.AreEqual("Albania", result[0].Name);
+        }
+    }
+}
